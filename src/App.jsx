ad97f9ddf650f5f3dec9afca78ac6b4fee21ad44
AvaCode69 <reacthmd34@gmail.com>
2023-04-26 18:02:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="section-center">
+      <h4>tried of boring lorem ipsum? </h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
+};
+export default App;
